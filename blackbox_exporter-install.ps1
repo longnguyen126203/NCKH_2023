65feed8f5ef351ba3d6452609bac286fc4c1935d
LeonDinh <93775704+LeonDinh2805@users.noreply.github.com>
2023-12-19 20:42:44 +0700

    Update blackbox_exporter-install.ps1

--- blackbox_exporter-install.ps1
+++ blackbox_exporter-install.ps1
@@ -3,4 +3,4 @@ curl -o "blackbox_exporter-0.24.0.windows-amd64.zip" "https://github.com/prometh
 Expand-Archive .\blackbox_exporter-0.24.0.windows-amd64.zip .
 
 # Chay file
-.\blackbox_exporter-0.24.0.linux-amd64\blackbox_exporter.exe
+.\blackbox_exporter-0.24.0.windows-amd64\blackbox_exporter.exe
