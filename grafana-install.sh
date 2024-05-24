c4219b162083c64d416ad3d7dfeaaf55503f4886
Gonn Nguyen <113317914+longnguyen126203@users.noreply.github.com>
2024-05-24 10:43:19 +0700

    Update grafana-install.sh

--- grafana-install.sh
+++ grafana-install.sh
@@ -6,10 +6,10 @@ tar -zxvf 'grafana-enterprise-10.3.3.linux-amd64.tar.gz'
 rm 'grafana-enterprise-10.3.3.linux-amd64.tar.gz'
 
 # Create grafana user
-#useradd -M -s /bin/false grafana
+useradd -M -s /bin/false grafana
 
 # Give file ownership to user node_exporter
-#chown -R grafana:grafana 'grafana-11.0.0'
+chown -R grafana:grafana 'grafana-10.3.3'
 
 # Move the files to /usr/local/bin/
 mv 'grafana-v10.3.3'/bin/* /usr/local/bin/
@@ -22,8 +22,8 @@ Wants=network-online.target
 After=network-online.target
 		
 [Service]
-#User=grafana
-#Group=grafana
+User=grafana
+Group=grafana
 Type=simple
 ExecStart=/usr/local/bin/grafana server \
 --config.file /etc/grafana.ini
